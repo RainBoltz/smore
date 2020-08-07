48757801ca62d0ac27641f9d60bff9b27d2b80e2
Philip Wang <RainBoltz@users.noreply.github.com>
2020-08-07 15:13:25 +0800

    enable OpenMP support

--- compile_all.bat
+++ compile_all.bat
@@ -1,5 +1,5 @@
-for %%x in (src/*.cpp) do cl /c /EHsc /D_XKEYCHECK_H src/%%x
-for %%x in (src/model/*.cpp) do cl /c /EHsc /D_XKEYCHECK_H src/model/%%x
+for %%x in (src/*.cpp) do cl /c /EHsc /D_XKEYCHECK_H /openmp src/%%x
+for %%x in (src/model/*.cpp) do cl /c /EHsc /D_XKEYCHECK_H /openmp src/model/%%x
 mkdir bin
 lib /out:bin/proNet.lib *.obj
 del /f *.obj
@@ -8,7 +8,7 @@ set s=deepwalk line walklets hpe app mf bpr hoprec warp nemf nerank
 set t=%s%
 :loop
 for /f "tokens=1*" %%a in ("%t%") do (
-   cl /c /EHsc /D_XKEYCHECK_H cli/%%a.cpp
+   cl /c /EHsc /D_XKEYCHECK_H /openmp cli/%%a.cpp
    cl %%a.obj proNet.lib /Febin\%%a.exe /link /LIBPATH:bin
    del /f %%a.obj
    set t=%%b
